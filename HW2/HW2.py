53a419d7acbc0a43bbe0dbb4022bf4f5b357ef12
Idan Hershcovich <idanhershcovich@idans-mbp.lan>
2020-02-09 02:29:38 -0500

    Finished HW2

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -1,111 +1,195 @@
 import os
 import numpy as np
 import random
-# from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA
 import matplotlib
 matplotlib.use("Agg")
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import math
+import io
+from PIL import Image
+from mpl_toolkits.mplot3d import Axes3D
+import glob
+import cv2
+
+
+
+#constants
 random.seed(0)
+e = 2**(-23)
 
 
-def euclidean(centroid, observation):
-    distance =math.sqrt(
-        ((centroid.item(0)-observation.item(0))**2)
-        + ((centroid.item(1)-observation.item(1))**2)
-        + ((centroid.item(2)-observation.item(2))**2)
-        )
-    return distance
-
-def standardize(data):
-   stand = (data-data.mean(axis=(0), keepdims=False))/data.std(axis=(0,1), keepdims=False, ddof=1)
-   return stand, data.mean(axis=(0), keepdims=False), data.std(axis=(0,1), keepdims=False, ddof=1)
-
-def pca(X, dims):
-    # Data matrix X, assumes 0-centered
-    n, m = X.shape
-    assert np.allclose(X.mean(axis=0), np.zeros(m))
-    # Compute covariance matrix
-    C = np.cov(X, rowvar=0, ddof=1)
-    # Eigen decomposition
-    eigen_vals, eigen_vecs = np.linalg.eig(C)
-
-    # Sorts eigenvalues and eigen vectors. Returns the most signifacnt eigen vals/vec. Quantity of dimensions specified with the dims argument.
-    idx = eigen_vals.argsort()[-dims:][::-1]
-    eigen_vals = eigen_vals[idx]
-    eigen_vecs = eigen_vecs[:,idx]
-
-    # Project X onto PC space
-    X_pca = np.dot(-X, eigen_vecs)
-    return X_pca
-
-
+#Data needs to be in the SAME DIRECTORY as python file
 data = np.genfromtxt('diabetes.csv', delimiter=',')
 
-###Fitting the data
-class_label_Y = data[:,0]
-obs_data_X, x_mean, x_std = standardize(data[:,1:])
+
+#HW1 standardize
+def standardize(data):
+   stand = (data)
+   test = data.mean(axis=(0), keepdims=False)
+   test2 = data.std(axis=(0), keepdims=False)
+
+   stand = (stand-test) / test2
+   return stand
+
+#Euclidean between a reference vector and an observation
+def euclidean(centroid, observation):
+    distance =math.sqrt(
+        (np.power((centroid[0]-observation[0]),2))+ ((np.power((centroid[1]-observation[1]),2)))+ ((np.power((centroid[2]-observation[2]),2))))
+    return distance
 
 
+#Manhattan distance between old reference vector and new
+def manhattan_distance(old, new):   
+    temp_sum = 0
+    for axis in range(len(old)):
+        temp_sum  += abs((old[axis]) - (new[axis]))
+    return temp_sum
+   
+
+##Function for updating the reference vectors. Takes the cluster and current ref vectors. Creates a new dictionary based on the Key and K
+def update(cluster, centroids):
+    updated_centroids = dict()
+    
+    for key in centroids.keys():
+        obs = []
+        ref = []
+        cluster_data = [x[1] for x in cluster[key]]
+        
+        for axis in range(len(centroids[1])):
+            for val in range(len(cluster_data)):
+                obs.append(cluster_data[val][axis])
+            mean =  sum(obs)/len(obs)
+            ref.append(mean)
+        updated_centroids.update({key: ref})
+
+    return updated_centroids
+
+
+# Calculates the clustering for each observation by calculating the Euclid. dist. and then assigning values to the min dist at a specific index.
+def clustering(data, centroids):
+    clusters = {
+            i: []
+            for i in centroids.keys()
+        }
+    for obs in range(np.size(data,axis=0)): 
+        min_dist = 10000
+        min_index = 0
+        for i in centroids.keys():
+            
+            dist = euclidean(centroids[i], data[obs])
+            if dist < min_dist: 
+               
+                min_dist = dist
+                min_index = i
+        clusters[min_index].append((obs, data[obs]))
+    
+    return clusters
+
+#Plotting using matplot lib. Cycling through colors. Making video
+def plot(clusters,name,purity):
+   
+    if os.path.isdir('./tempImages') == False:
+        dir = os.mkdir('./tempImages')
+    else:
+        dir = './tempImages/'
+    fig = plt.figure()
+    colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'w']
+    ax = fig.add_subplot(111, projection='3d')
+    ass = str(purity)
+    ax.set_title(" Purity = " + ass)
+
+    for i in clusters.keys():
+        for vals in range(len(clusters[i])):
+            ax.scatter(clusters[i][vals][1][0], clusters[i][vals][1][1], clusters[i][vals][1][2], c = colors[i], marker='o')
+        
+       
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.savefig(str(dir) + str(name) + '.jpg')
+    plt.close()
+
+def video():
+    img_array = []
+    os.chdir('./tempImages')
+    
+    for filename in glob.glob("*.jpg"):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        img_array.append(img)
+
+    out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 15, (width, height),isColor=True)
+
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
+    cv2.destroyAllWindows()
+
+# calculating purity by checking the Y labels
+def purity(clusters, labels):
+    calc = 0
+    for keys in clusters.keys():
+        one = 0
+        negative_one = 0
+        for obs in range(len(clusters[keys])):
+            index = clusters[keys][obs][0]
+            if labels[index] == -1:
+                negative_one +=1
+            else:
+                one +=1
+        maxi = (len(clusters[keys])*(max(one,negative_one))) / len(clusters[keys])
+        calc += maxi
+    calc = calc/(len(labels))
+
+
+    return calc
+    
+# Main driver function. Runs PCA, initializes reference vectors, and runs the clustering until convergence. Includes the plotting
 def myKMeans(data_clusterX, target_clusterY, k):
-    ref_vector = np.empty([0,3], int)
-    print(str.format("shape of empty ref vector {}",ref_vector.shape))
-
-    print(str.format("shape of cluster {}",data_clusterX.shape))
+    
     if np.size(data_clusterX,1) > 3:
-        data_clusterX = pca(data_clusterX, 3)
-        print(str.format("shape of cluster after pca {}",data_clusterX.shape))
-
-    # Get my reference vectors
-    for i in range(k):
-        rand = random.randrange(0,np.size(data_clusterX,axis=0))
-        rand = np.column_stack(data_clusterX[rand])
-        # ref_vector = np.append(ref_vector, rand, axis=0)
+        pca = PCA(n_components=3)
+        data_clusterX = pca.fit_transform(data_clusterX)
 
     ##dictionary for the reference vectors.  The value comes from a random observation in the data.
     centroids = {
-        i+1: np.column_stack(data_clusterX[random.randrange(0,np.size(data_clusterX,axis=0))])
+        i+1: data_clusterX[random.randrange(0,np.size(data_clusterX,axis=0))]
         for i in range(k)
     }
 
-    # ##Figure 
-    # fig = plt.figure(figsize=(5, 5))
-    # colmap = {1: 'r', 2: 'g', 3: 'b'}
-    # for i in centroids.keys():
-    #     plt.scatter(*centroids[i], color=colmap[i])
+    sums = 10
+    iters = 0
+    frames = 20
 
-    # print(centroids.keys())
-    # print(centroids[1].item(0))
-    # print(data_clusterX[0].item(0))
-    # print(np.size(data_clusterX,axis=0))
-
-    
-    
-    for obs in range(np.size(data_clusterX,axis=0)):
-        min = 1000
-        min_index = 0
-        for i in centroids.keys():
-            dist = euclidean(centroids[i], data_clusterX[obs])
+    #Loop until convergence (currently set to a lower number so it wouldnt take too long)
+    while sums > e:
+        if iters < frames:
+            clusters_data = clustering(data_clusterX, centroids )
+            pure = purity(clusters_data, class_label_Y)
+            plot(clusters_data, sums, pure)
             
-            if dist < min: 
-               
-                min = dist
-                min_index = i
-                
-               
-            print(str.format("distance from observation {} to centroid {} is: {}", obs+1 , i, dist))
-        print(str.format(" observation {} belongs to centroid {} with distance: {}", obs+1, min_index, min))
+            centroids_updated = update(clusters_data, centroids)
+            sums = 0
+            for key in centroids.keys():
+                sums +=manhattan_distance(centroids[key], centroids_updated[key])
 
-        ###Put data in clusters. Each cluster should have the observation that belongs to it. Stuck here
-        clusters = {
-            min_index: (obs+1,min )
-            for i in range(obs)
-        }
-    print(clusters.items())
-    
+            centroids = centroids_updated
+            iters += 1
+            print(str.format("frames left: {}" (40-iters)))
+        else:
+            video()
+           
+            break
 
-print(str.format("shape of label {}", class_label_Y.shape))
-print(str.format("shape of obs_data {}", obs_data_X.shape))
+            
+###Fitting the data
+class_label_Y = data[:,0]
+obs_data_X = standardize(data[:,1:])
+
 
 myKMeans(obs_data_X, class_label_Y, 7)
 
---
+++ HW2/README.txt
@@ -0,0 +1,6 @@
+This file needs the diabetes.csv database in the same directory in order to work!!
+
+The way this program works is that it will create plots based on the clustering from the code. The program takes a while to create all the
+images and then the video. It creates 100 frames for the video. The process of the image creation is stored in tempImages. IT WILL TAKE A WHILE! 
+To reduce the number of plots for testing or grading, change the "frames" variable in line 166 to a smaller number. The program can also run until
+convergance, but that would take longer. Its turned off for testing and grading purposes
