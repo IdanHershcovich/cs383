727da55a83d21651d6f0323d59ec5ce7714e2119
Idan Hershcovich <idanhershcovich@idans-mbp-16.lan>
2020-03-03 01:09:21 -0500

    Work on part 2. Classified data into spam and not spam

--- HW4-Classification/bayes.py
+++ HW4-Classification/bayes.py
@@ -1,7 +1,10 @@
-import numpy as np
-import random
 import math
+import random
+
+import numpy as np
 from sklearn.model_selection import train_test_split
+
+np.random.seed(0)
 np.set_printoptions(suppress=True)
 
 def standardize(data):
@@ -12,20 +15,40 @@ def standardize(data):
    stand = (stand-test) / test2
    return stand
 
-
-def bayes(data):
+def parseAndClassify(data):
+	# get data from file
 	full_data = np.genfromtxt(data, delimiter=',')
-	features = full_data[:,:-1]
-	label = full_data[:,-1]
+	# shuffle
+	np.random.shuffle(full_data)
 
-	#split into training and testing arrays. 2/3 is training
-	xTrain, xTest, yTrain, yTest = np.asarray(train_test_split(features, label, train_size = 0.7, random_state = 0))
+	# make a copy of the full data to make the separation of training (2/3) and testng (1/3) 
+	temp = full_data[:]
+	training_data = temp[0:math.ceil(np.size(temp,axis=0)*(2/3))]
+	temp = np.delete(temp,slice(0,np.size(training_data,axis=0)), axis=0)
+	testing_data = temp	
 
-	xTrain=standardize(xTrain)
-	yTrain=standardize(yTrain)
-	import pdb
-	pdb.set_trace()
+	#Separate into spam and not spam
+	training_stand = standardize(training_data[:,:-1])
+	spam = []
+	not_spam = []
+	for i in range(np.size(training_stand,axis=0)):
+		if(training_data[i,-1:]) == 1:
+			
+			spam.append(training_stand[i])
+		else:
+			
+			not_spam.append(training_stand[i])
 
+
+	return training_data, testing_data, spam, not_spam
+
+
+def bayes(data):	
+
+	tr,te, spam, n_spam = parseAndClassify(data)
+	
+	
+	import pdb; pdb.set_trace()
 	
 
 
